ff082019f8fd1a0eedd80ebbd8a45cb3fcc9490b
Mohan Das <mohandaskj.11@gmail.com>
2026-01-18 22:06:45 +0530

    Initial Commit

---
+++ app/src/main/java/com/deddecode/gostreak/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.deddecode.gostreak
+
+import android.R
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.deddecode.gostreak.ui.theme.GoStreakTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            HomeAppBar()
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeAppBar(){
+    TopAppBar(
+        title = {
+            Text("Go Streak")
+        }
+    )
+}
